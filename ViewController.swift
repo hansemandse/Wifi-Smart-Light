31726c4552144cc75cdfd441843e59545a272a77
hansemandse <37290815+hansemandse@users.noreply.github.com>
2018-04-06 17:21:53 +0200

    Add files via upload
    Functional app that connects, disconnects and sends messages to the MQTT server. Reading out slider values and textfield strings is still not functional.

--- ViewController.swift
+++ ViewController.swift
@@ -1,44 +1,51 @@
+//
+//  ViewController.swift
+//  Smart Light Controller
+//
+//  Created by Hans Jakob on 06/04/2018.
+//  Copyright © 2018 DTU. All rights reserved.
+//
 import UIKit
 import CocoaMQTT
 
-class UIViewController {
-	// Constants for the controller
-	let clientID = "iOS Controller"
-	let host = "192.168.0.xxx"
-	let port = 1883
-	
-	// New MQTT client
-	let mqttClient = CocoaMQTT(clientID: "INDSÆT", host: "INDSÆT", port: INDSÆT)
-	
-	// Setup functions
-	override func viewDidLoad() {
-		super.viewDidLoad()
-	}
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-	}
-	
-	// ON/OFF switch method
-	@IBAction func stateSwitch(_ sender: UISwitch) {
-		var sliderValue: String = String(Int(sender.value))
-		if sender.isOn {
-			mqttClient.publish("rpi/gpio", withString: "on")
-		} else {
-			mqttClient.publish("rpi/gpio", withString: "off")
-		}
-	}
-	
-	// Dimming slider method
-	@IBAction func dimSlider(_ sender: UISlider) {
-		var sliderValue: String = String(Int(sender.value))
-		mqttClient.publish("rpi/gpio", withString: "dc " + sliderValue)
-	}
-	
-	// Connect and Disconnect methods
-	@IBAction func buttonConnect(_ sender: UIButton) {
-		mqttClient.connect()
-	}
-	@IBAction func buttonDisconnect(_ sender: UIButton) {
-		mqttClient.disconnect()
-	}
+class ViewController: UIViewController {
+    // New MQTT client
+    let mqttClient = CocoaMQTT(clientID: "iOS Controller", host: "172.20.10.2", port: 1883)
+    
+    // Setup functions
+    override func viewDidLoad() {
+        mqttClient.publish("rpi/gpio", withString: "dc 50")
+        super.viewDidLoad()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // ON/OFF switching method
+    @IBAction func stateSwitch(_ sender: UISwitch) {
+        if (sender.isOn) {
+            mqttClient.publish("rpi/gpio", withString: "on")
+        } else {
+            mqttClient.publish("rpi/gpio", withString: "off")
+        }
+    }
+    
+    // Managing the input from the text field
+    @IBAction func enterIP(_ sender: UITextField) {
+        // TODO: Add reading out the entered IP-address and opening a mqttClient based on this (rather than opening it in the beginning of the program
+    }
+    
+    // Dimming slider method
+    @IBAction func dimSlider(_ sender: UISlider) {
+        // TODO: Fix the reading of the slider value
+        mqttClient.publish("rpi/gpio", withString: "dc " + String(Int(sender.value)))
+    }
+    
+    // Connect and Disconnect methods
+    @IBAction func connectButton(_ sender: UIButton) {
+        mqttClient.connect()
+    }
+    @IBAction func disconnectButton(_ sender: UIButton) {
+        mqttClient.disconnect()
+    }
 }
